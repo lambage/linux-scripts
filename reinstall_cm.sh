c45aa16bab6f8d6cb3f546389a33b5c1ee7cff10
Kris Lamb <kris.lamb@dejero.com>
2018-05-15 08:17:40 -0400

    got rid of commented out line

--- reinstall_cm.sh
+++ reinstall_cm.sh
@@ -3,5 +3,4 @@ sudo systemctl stop cm.socket cm-memod cm-autodial cm
 sudo rm /run/dejero/connection_manager.socket
 sudo dpkg -i igw-connection-manager_0.0.0_amd64.deb
 sudo systemctl start cm.socket cm-memod cm-autodial cm
-#sudo systemctl start cm-memod cm-autodial cm
 
